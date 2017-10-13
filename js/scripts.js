fdfce2e641948df16f1d1afddec08b18b39cb649
alex2801 <vitalik.makoed@mail.ru>
2017-10-14 01:12:01 +0300

    First commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,27 @@
 var check = function(word) {
-	var total = 0;
-	word.split(/\s*/).forEach(function(word) {
-		if (word === 'q' || word === 'z') {
-			 total += 10;
-		} else if (word === 'j' || word === 'x') {
-			 total += 8;
-		} else if (word === 'k') {
-			 total += 5;
-		} else if (word === 'f' || word === 'h' || word === 'v' || word === 'w' || word === 'y') {
-			 total += 4;
-		} else if (word === 'b' || word === 'c' || word === 'm' || word === 'p') {
-			 total += 3;
-		} else if (word === 'd' || word === 'g') {
-			 total += 2;
-		} else if (word === 'a' || word === 'e' || word === 'i' || word === 'o' || word === 'u' || word === 'l' || word === 'n' || word === 'r' || word === 's' || word === 't') {
-		 total += 1;
+		var total = 0;
+		var arr = word.toLowerCase().split(/\s*/);
+		var patterns = [/[aeioulnrst]/, /[dg]/, /[bcmp]/, /[fhvwy]/, /[k]/, /[jx]/, /[qz]/];
+		
+		for (var i = 0; i < arr.length; i++) {
+		
+				patterns.forEach(function(pattern, ind) {
+						total += (pattern.test(arr[i]) && ind <= 4) ? ind + 1 : (pattern.test(arr[i]) && ind === 5) ? 8 : (pattern.test(arr[i]) && ind === 6) ? 10 : 0;
+				});
+			
 		}
-	});
-	return total;
+		return total;
 }
 
 $(document).ready(function() {
-	$("form#scrabble").submit(function(event) {
-		var word = $("input#word").val().trim().toLowerCase();
-		var result = check(word);
-		
-		$(".points").text(result);
-		$(":input").val("");
-		$("#result").show();
-		
-		event.preventDefault();
-	});
+		$("form#scrabble").submit(function(event) {
+				var word = $("input#word").val().trim();
+				var result = check(word);
+				
+				$(".points").text(result);
+				$(":input").val("");
+				$("#result").show();
+				
+				event.preventDefault();
+		});
 });
